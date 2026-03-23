c8e93b07f1bee4f4650663af566ec979da90dabe
Idmona <tanya.nemchenko23.85@gmail.com>
2026-03-23 19:36:07 +0300

    Добавлен вывод вин из Excel

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import datetime
+import pandas as pd
+
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
@@ -21,13 +23,19 @@ def get_year_phrase(n: int) -> str:
             word = "лет"
     return f"Уже {n} {word} с вами!"
 
+
+df = pd.read_excel('wine.xlsx')
+wines = df.to_dict(orient='records')
+
+
 env = Environment(
     loader=FileSystemLoader('.'),
     autoescape=select_autoescape(['html'])
 )
 template = env.get_template('template.html')
 rendered_page = template.render(
-    years_text=get_year_phrase(delta_years)
+    years_text=get_year_phrase(delta_years),
+    wines=wines
 )
 
 with open('index.html', 'w', encoding="utf8") as file:
