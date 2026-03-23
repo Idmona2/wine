77685b431ff3867e007c1a7e50457a2ace9d306e
Idmona <tanya.nemchenko23.85@gmail.com>
2026-03-23 17:38:36 +0300

    Вывела возраст винодельни

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+import datetime
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 
+current_date = datetime.date.today()
+event_date = datetime.date(year=1920, month=1, day=1)
+
+delta_years = current_date.year - event_date.year
+
+env = Environment(
+    loader=FileSystemLoader('.'),
+    autoescape=select_autoescape(['html'])
+)
+template = env.get_template('template.html')
+rendered_page = template.render(years_diff=delta_years)
+
+with open('index.html', 'w', encoding="utf8") as file:
+    file.write(rendered_page)
 
 server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
 server.serve_forever()
