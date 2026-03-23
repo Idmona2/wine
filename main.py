15d25236134d6f720315c6949f4c3975a09dbadb
Idmona <tanya.nemchenko23.85@gmail.com>
2026-03-23 17:46:01 +0300

    Внесла изменения в надпись год/лет/года

--- main.py
+++ main.py
@@ -7,12 +7,26 @@ event_date = datetime.date(year=1920, month=1, day=1)
 
 delta_years = current_date.year - event_date.year
 
+def get_year_phrase(n: int) -> str:
+    n = abs(n)
+    if 11 <= n % 100 <= 14:
+        word = "лет"
+    else:
+        last_digit = n % 10
+        if last_digit == 1:
+            word = "год"
+        elif 2 <= last_digit <= 4:
+            word = "года"
+        else:
+            word = "лет"
+    return f"Уже {n} {word} с вами!"
+
 env = Environment(
     loader=FileSystemLoader('.'),
     autoescape=select_autoescape(['html'])
 )
 template = env.get_template('template.html')
-rendered_page = template.render(years_diff=delta_years)
+rendered_page = template.render(years_text=year_phrase)
 
 with open('index.html', 'w', encoding="utf8") as file:
     file.write(rendered_page)
